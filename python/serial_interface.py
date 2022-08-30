2812f811d513dca1995be3cd7d212629820f7cd1
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2022-08-30 18:53:00 +0900

    feat: Update python serial_interface, now works

--- python/serial_interface.py
+++ python/serial_interface.py
@@ -1,43 +1,14 @@
-# Interface setup adapted from https://problemsolvingwithpython.com/
-
-# Slack overflow Mac port commands
-# You can find your Arduino via Terminal with
-
-#     ls /dev/tty.*
-
-# then you can read that serial port using the screen command, like this
-
-#     screen /dev/tty.[yourSerialPortName] [yourBaudRate]
-
-# for example:
-
-#     screen /dev/tty.usbserial-A6004byf 9600
-
-
-# serialInterface.py
+#Find Arduino via Terminal with # ls /dev/tty.*
 
 import serial
 import time
 
-ser = serial.Serial('/dev/cu.usbserial-FTDK64TL', 9600)
-time.sleep(2)
-b = ser.readline() #readline() returns one line from a file
-b
-#b'409\r\n'
-type(b)
-#<class 'bytes'>
-str_rn = b.decode()
-str_rn
-#'409\r\n'
-str = str_rn.rstrip()
-str
-#'409'
-type(str)
-#<class 'str'>
-f = float(str)
-#409.0
-type(f)
-#<class 'float'>
-print(f)
-#ser.close()
-#exit()
+ser = serial.Serial('/dev/cu.usbserial-FTDK64TL', 9600) #port name, baud rate
+
+def read_serial_data():
+    data = ser.readline() #read line of data
+    print(data.decode().rstrip()) # decode and strip r/n char
+    return data.decode().rstrip()
+
+# ser.close()
+# exit()
