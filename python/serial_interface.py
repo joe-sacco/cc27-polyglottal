7b5f8ad3811e5dd528bdafed97e2f015a6261ddb
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2022-08-31 14:47:51 +0900

    feat: Update serial_interface

--- python/serial_interface.py
+++ python/serial_interface.py
@@ -1,9 +1,21 @@
-#Arduino address in Terminal type 'ls /dev/tty.*'
+#For arduino address in Terminal type 'ls /dev/tty.*'
 
 import serial
 import time
+import os
+import pty
 
-ser = serial.Serial('/dev/cu.usbserial-FTDK64TL', 9600) #port name, baud rate
+# primary, secondary = pty.openpty()
+# if '/dev/cu.usbserial-FTDK64TL' == True:
+#     PORT = '/dev/cu.usbserial-FTDK64TL' # arduino usb port
+# else: 
+#     PORT = os.ttyname(secondary) #virtual test port
+
+PORT = '/dev/cu.usbserial-FTDK64TL'
+# print('🟡', PORT)
+
+ser = serial.Serial(PORT, 9600) #port name, baud rate
+time.sleep(1)
 
 def read_serial_data():
     data = ser.readline() #read line of data
@@ -12,3 +24,20 @@ def read_serial_data():
 
 # ser.close()
 # exit()
+
+
+# import serial
+# import time
+
+
+
+# BELOW CODE WORKS WHEN ARDUINO IS HOOKED UP!!
+
+# PORT = '/dev/cu.usbserial-FTDK64TL' # arduino usb port
+# ser = serial.Serial(PORT, 9600) #port name, baud rate
+# time.sleep(1)
+
+# def read_serial_data():
+#     data = ser.readline() #read line of data
+#     print(data.decode().rstrip()) 
+#     return data.decode().rstrip() # decode and strip r/n char
