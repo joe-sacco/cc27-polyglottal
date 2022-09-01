ea2571018b1f8ad9600b30f8537fd68dfca77e58
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2022-09-01 14:44:48 +0900

    feat: Update arduino serial_interface settings

--- arduino/AnalogReadSerial/AnalogReadSerial.ino
+++ arduino/AnalogReadSerial/AnalogReadSerial.ino
@@ -9,7 +9,7 @@
 
   This example code is in the public domain.
 
-  
+
 */
 
 // the setup routine runs once when you press reset:
@@ -20,11 +20,27 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0); 
-  // print out the value you read:
-  sensorValue = sensorValue - 11; // sensor range offset
-  Serial.println(constrain(sensorValue, 0, 127)); // constrained to MIDI note range
-  //Serial.println(sensorValue);
+  //read the input on analog pin 0:
+  int sensorValue1 = analogRead(A0);
+  //print out the value you read:
+  sensorValue1 = sensorValue1 - 11; // sensor range offset
+
+  Serial.println(constrain(sensorValue1, 0, 100)); // constrained for parsing by 10 later in Python
   delay(50);        // delay in between reads for stability
 }
+
+
+
+
+  //int sensorValue2 = analogRead(A5);
+  //sensorValue2 = sensorValue2 - 11; // sensor range offset
+  //int dataStream[2] = {sensorValue1, sensorValue2};
+  //Serial.println(constrain(sensorValue2, 0, 127)); // constrained to MIDI note range
+  //Serial.println(dataStream[2]);
+  
+//  for (int pin = 0; pin < 3; pin++) {
+//    char A = 'A';
+//    int dummy = analogRead(A0);
+//    int value = analogRead(A + pin);
+//    Serial.println(value);
+//  }
